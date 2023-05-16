39e5af918b9d780155ee66cfbffe898d16671251
Rahul Kumar <rahul1.shc@gmail.com>
2023-05-16 13:33:01 +0530

    js sort

--- Sorting/javascriptSort.js
+++ Sorting/javascriptSort.js
@@ -2,6 +2,18 @@ function compare ( a, b) {
     return a - b;
 }
 
+function compareByLength ( a, b ){
+    return a.length - b.length;
+}
+
+function compareByLengthReverse ( a, b ){
+    return b.length - a.length;
+}
+
 console.log( [ 1, 7, -12, 10, 0, -2 ].sort(  ) ); //string based sort based on unicode
 
 console.log( [ 1, 7, -12, 10, 0, -2 ].sort( compare ) );
+
+console.log( [ "abc", "bcde", "edfgggh", "a", "ab"].sort( compareByLength ) );
+
+console.log( [ "abc", "bcde", "edfgggh", "a", "ab"].sort( compareByLengthReverse ) );
